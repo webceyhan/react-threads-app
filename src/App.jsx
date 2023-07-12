4cefa925120dd32ce236db5da267eaf355896d7a
Evren Ceyhan <evren@ceyhan.io>
2023-07-12 23:34:51 +0200

    basic components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+const Nav = () => <nav>Nav</nav>;
+
+const Header = () => <header>Header</header>;
+
+const Thread = () => <article>Thread</article>;
+
+const ThreadForm = () => <form>ThreadForm</form>;
+
+const Feed = () => (
+    <main>
+        <Thread />
+        <Thread />
+        <Thread />
+    </main>
+);
+
+const SlideUp = () => (
+    <div>
+        <Thread />
+        <Thread />
+    </div>
+);
+
 export default function App() {
-    return <>hello world</>;
+    return (
+        <>
+            <Nav />
+            <Header />
+            <Feed />
+            <ThreadForm />
+            <SlideUp />
+        </>
+    );
 }
