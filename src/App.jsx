d454325f3c34f3d9f629a96bca1f4fe053abf46a
Evren Ceyhan <evren@ceyhan.io>
2023-07-12 23:44:02 +0200

    threadform moved into slideup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Feed from './components/Feed';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import SlideUp from './components/SlideUp';
-import ThreadForm from './components/ThreadForm';
 
 export default function App() {
     return (
@@ -10,7 +9,6 @@ export default function App() {
             <Nav />
             <Header />
             <Feed />
-            <ThreadForm />
             <SlideUp />
         </>
     );
--- src/components/SlideUp.jsx
+++ src/components/SlideUp.jsx
@@ -1,10 +1,12 @@
 import Thread from './Thread';
+import ThreadForm from './ThreadForm';
 
 export default function SlideUp() {
     return (
         <div>
             <Thread />
             <Thread />
+            <ThreadForm />
         </div>
     );
 }
