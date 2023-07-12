18347bb56d84aede87609bc9392da042d9212c71
Evren Ceyhan <evren@ceyhan.io>
2023-07-13 00:42:51 +0200

    header styles added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,37 @@
 export default function Header() {
-    return <header>Header</header>;
+    return (
+        <header>
+            <div className="info-container">
+                <div className="user-info-container">
+                    <h1>user name</h1>
+                    <p>
+                        handle <span className="threads-info">threads.net</span>
+                    </p>
+                </div>
+                <div className="img-container">
+                    <img src="" alt="avatar" />
+                </div>
+            </div>
+
+            <p>bio</p>
+
+            <div className="stats-container">
+                <p className="subtext">
+                    X followers - <a href="#">link</a>
+                </p>
+            </div>
+
+            <button
+                className="primary"
+                onClick={() => navigator.clipboard.writeText('my profile url')}
+            >
+                Share Profile
+            </button>
+
+            <div className="button-container">
+                <button className="active">Threads</button>
+                <button>Replies</button>
+            </div>
+        </header>
+    );
 }
