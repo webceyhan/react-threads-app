e6e048dcae6dc1b3a5123df1668fc8ee7b9c46a4
Evren Ceyhan <evren@ceyhan.io>
2023-07-13 13:42:03 +0200

    extra space from top added

--- src/components/ThreadForm.jsx
+++ src/components/ThreadForm.jsx
@@ -1,6 +1,8 @@
 export default function ThreadForm({ user, text, setText, postThread }) {
     return (
         <>
+            <br />
+
             <p>
                 Post or reply as <strong>@{user.handle}</strong>
             </p>
