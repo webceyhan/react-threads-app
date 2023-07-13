0e8ffc7f6564770cc122fbbd38a3c5e6836d9212
Evren Ceyhan <evren@ceyhan.io>
2023-07-13 12:22:42 +0200

    reply threads user data fetched

--- src/components/SlideUpThread.jsx
+++ src/components/SlideUpThread.jsx
@@ -1,15 +1,28 @@
-import { ago } from '../utils';
+import { useEffect, useState } from 'react';
+import { ago, fetchApi } from '../utils';
 
-export default function SlideUpThread({ user, thread }) {
+export default function SlideUpThread({ thread }) {
+    const [user, setUser] = useState(null);
+
+    const loadUser = async () => {
+        // this returns an array of users matching the query
+        const users = await fetchApi(`users?uuid=${thread.from}`);
+        setUser(users[0]); // so we fetch the first user
+    };
+
+    useEffect(() => {
+        loadUser();
+    }, []);
+
     return (
         <article className="feed-card">
             <div className="text-container">
                 <div className="user-info">
                     <div className="img-container">
-                        <img src={user.avatarUrl} alt="avatar" />
+                        <img src={user?.avatarUrl} alt="avatar" />
                     </div>
                     <p>
-                        <strong>{user.handle}</strong>
+                        <strong>{user?.handle}</strong>
                     </p>
                 </div>
                 <p className="subtext">{ago(thread.timestamp)}</p>
